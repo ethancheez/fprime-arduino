7023f02ddcfdf33bc1e175cb224e8c834cc98ffc
Ethan Chee <ethancheez@gmail.com>
2023-09-17 15:07:04 -1000

    bump

--- cmake/toolchain/thingplusrp2040.cmake
+++ cmake/toolchain/thingplusrp2040.cmake
@@ -11,5 +11,6 @@ set(FPRIME_USE_BAREMETAL_SCHEDULER ON)
 
 
 set(ARDUINO_FQBN "rp2040:rp2040:sparkfun_thingplusrp2040")
+add_compile_options(-DUSE_BASIC_TIMER)
 # Run the base arduino setup which should detect settings!
 include("${CMAKE_CURRENT_LIST_DIR}/support/arduino-support.cmake")
